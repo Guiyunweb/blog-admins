77f896387086f81897f0fc6eed15cbb088c47f05
Guiyun <i@guiyunweb.com>
2020-06-19 18:00:16 +0800

    feat:添加上传接口

--- src/views/article/edit/page.vue
+++ src/views/article/edit/page.vue
@@ -4,7 +4,7 @@
       <div style="float: right;">
         <el-button size="small" @click="save(false)" type="danger">保存草稿</el-button>
         <el-button size="small" type="info">预览</el-button>
-        <el-button size="small" type="primary">发布</el-button>
+        <el-button size="small" @click="drawer = true" type="primary">发布</el-button>
       </div>
     </template>
     <el-form>
@@ -17,6 +17,34 @@
         class="mde"/>
       </el-form-item>
     </el-form>
+    <el-drawer
+      title="文章设置"
+      :visible.sync="drawer"
+      >
+      <el-form label-position="top" style="margin: 15px">
+        <el-form-item label="标题">
+          <el-input />
+        </el-form-item>
+        <el-form-item label="缩略图">
+          <el-upload
+            drag
+            style="width: 100%"
+            name="file"
+            :data="{output:'json',scene:'image'}"
+            :on-success="handleAvatarSuccess"
+            action="http://47.100.105.136:8080/group1/upload"
+            multiple>
+            <div v-if="!thumbnail">
+              <i class="el-icon-upload"></i>
+              <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+            </div>
+            <div v-else>
+              <img :src="'http://47.100.105.136:8080'+thumbnail" />
+            </div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+    </el-drawer>
   </d2-container>
 </template>
 
@@ -26,10 +54,12 @@ export default {
   name: 'edit',
   data () {
     return {
+      drawer: false,
       article: {
         title: null,
         content: null,
-        isRelease: false
+        isRelease: false,
+        thumbnail: null
       }
     }
   },
@@ -45,7 +75,15 @@ export default {
           })
         }
       })
+    },
+    handleAvatarSuccess (res, file) {
+      this.thumbnail = res.path
     }
   }
 }
 </script>
+<style>
+.drawer-form-item{
+  text-align: center;
+}
+</style>
